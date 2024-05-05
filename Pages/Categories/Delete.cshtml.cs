7e23c3b3869c41a9a125513f5ad19542a63007f7
yeillem fontal caicedo <yeillemfontalcaicedo12@gmail.com>
2024-05-05 14:58:57 -0500

    Delete Categories Correciones

--- Pages/Categories/Delete.cshtml.cs
+++ Pages/Categories/Delete.cshtml.cs
@@ -6,6 +6,7 @@ using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Categories
 {
+<<<<<<< HEAD
 	public class DeleteModel : PageModel
 	{
 		private readonly SupermarketContext _context;
@@ -55,3 +56,54 @@ namespace SupermarketWEB.Pages.Categories
 		}
 	}
 }
+=======
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category != null)
+            {
+                Category = category;
+                _context.Categories.Remove(Category);
+                await _context.SaveChangesAsync();
+
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
+>>>>>>> Delete Categories Correciones
