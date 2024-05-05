84a5eb23743eeb30c6d239217e0596290a19763c
yeillem fontal caicedo <yeillemfontalcaicedo12@gmail.com>
2024-05-05 17:11:20 -0500

    Edit Categories

--- Pages/Categories/Delete.cshtml.cs
+++ Pages/Categories/Delete.cshtml.cs
@@ -6,57 +6,6 @@ using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Categories
 {
-<<<<<<< HEAD
-	public class DeleteModel : PageModel
-	{
-		private readonly SupermarketContext _context;
-		public DeleteModel(SupermarketContext context)
-		{
-			_context = context;
-		}
-
-		[BindProperty]
-		public Category Category { get; set; }
-
-		public async Task<IActionResult> OnGetAsync(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
-
-
-			if (category == null)
-			{
-				return NotFound();
-			}
-
-			return Page();
-		}
-		public async Task<IActionResult> OnPostAsync(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var category = await _context.Categories.FindAsync(id);
-
-			if (category != null)
-			{
-				Category = category;
-				_context.Categories.Remove(Category);
-				await _context.SaveChangesAsync();
-
-			}
-
-			return RedirectToPage("./Index");
-		}
-	}
-}
-=======
     public class DeleteModel : PageModel
     {
         private readonly SupermarketContext _context;
@@ -106,4 +55,4 @@ namespace SupermarketWEB.Pages.Categories
         }
     }
 }
->>>>>>> Delete Categories Correciones
+
