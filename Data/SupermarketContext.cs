82639004c0e749fd948a7f5034ac192f0cbe24df
yeillem fontal caicedo <yeillemfontalcaicedo12@gmail.com>
2024-05-05 22:10:15 -0500

    Index PayMode

--- Data/SupermarketContext.cs
+++ Data/SupermarketContext.cs
@@ -10,6 +10,8 @@ namespace SupermarketWEB.Data
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<PayMode> PayModes { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
 
     }
 
